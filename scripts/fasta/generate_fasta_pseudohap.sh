49596efd09206a31964b901d9e53dd24723d64ea
Elias Michel Oziolor <eoziolor@farm.cse.ucdavis.edu>
2018-07-16 16:16:43 -0700

    made a psudohaploid version of the genome to run ARCS on

---
+++ scripts/fasta/generate_fasta_pseudohap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J fastagen
+#SBATCH -o /home/eoziolor/phgenome/scripts/fasta/phg_fasta_%j.o
+#SBATCH -e /home/eoziolor/phgenome/scripts/fasta/phg_fasta_%j.o
+#SBATCH --time=0-01:00
+#SBATCH --mem=60000
+
+my_super=/home/eoziolor/program/supernova-2.0.0/supernova
+my_out=/home/eoziolor/phgenome/data/assembly3/outs/assembly
+my_fasta=/home/eoziolor/phgenome/data/assembly3/fasta/phgenome
+
+$my_super mkoutput \
+--asmdir=$my_out \
+--outprefix=$my_fasta \
+--style=pseudohap \
+--headers=full
