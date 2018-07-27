4e891cc14e893fd3cd62cd638884c9c140a7ba4d
Elias Michel Oziolor <oziolor@gmail.com>
2018-07-27 12:27:06 -0700

    doing first iteration of arks+tigmint pipeline to avoid extra assembly errors; rather stringent

---
+++ scripts/tigarks/arks1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+
+#SBATCH -J runarks1
+#SBATCH -o runarks1-%j.o
+#SBATCH -e runarks1-%j.o
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --mem=60000
+#SBATCH --time=1-00:00
+#SBATCH --no-requeue
+#SBATCH -p high
+
+module load python3
+source ~/.bashrc
+
+cd /home/eoziolor/phgenome/data/tigarks/arks1/
+
+#files
+my_fasta=supernova
+my_reads=barcoded
+
+#code
+arks-make2 arks-tigmint \
+draft=$my_fasta \
+reads=$my_reads \
+m=50-10000 \
+a=0.5 \
+c=5 \
+e=10000 \
+r=0.05 \
+l=5 \
+z=250 \
+k=42 \
+threads=16 \
+t=16
