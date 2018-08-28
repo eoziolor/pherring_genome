ebfc73f3061164ab0c5c0f5438e702cdcdede732
Elias Michel Oziolor <oziolor@gmail.com>
2018-08-28 13:03:29 -0700

    running busco on metazoa as well (Andrew asked)

---
+++ scripts/busco/busco_met.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH -J arks_busco_met
+#SBATCH -o arks_busco_met-%j.o
+#SBATCH -e arks_busco_met-%j.o
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --mem=60000
+#SBATCH --time=2-00:00
+#SBATCH --no-requeue
+#SBATCH -p high
+#SBATCH -D /home/eoziolor/phgenome/data/busco/
+
+module load busco
+module load python3
+export AUGUSTUS_CONFIG_PATH=/home/eoziolor/phgenome/data/busco/config/
+
+my_run=/share/apps/busco-v2/BUSCO.py
+my_lineage=/home/eoziolor/phgenome/data/busco/metazoa_odb9
+my_genome=/home/eoziolor/phgenome/data/genome/phgenome_arks.fasta
+my_out=arks_busco_met
+
+
+python $my_run \
+-i $my_genome \
+-o $my_out \
+-l $my_lineage \
+-m genome \
+-c 16 \
+-sp zebrafish
