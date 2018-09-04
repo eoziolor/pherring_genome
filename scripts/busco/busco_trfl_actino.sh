6b15ef4ee6d286b2743d29d1ebc2ce76e334ac9f
Elias Michel Oziolor <oziolor@gmail.com>
2018-09-04 13:55:25 -0700

    Ran busco for actinopterygii on transcriptome

---
+++ scripts/busco/busco_trfl_actino.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+#SBATCH -J arks_busco_flnc_actino
+#SBATCH -o arks_busco_flnc_actino-%j.o
+#SBATCH -e arks_busco_flnc_actino-%j.o
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --mem=60000
+#SBATCH --time=1-00:00
+#SBATCH --no-requeue
+#SBATCH -p high
+#SBATCH -D /home/eoziolor/phgenome/data/transcriptome/
+
+module load busco
+module load python3
+module load bio3
+export AUGUSTUS_CONFIG_PATH=/home/eoziolor/phgenome/data/busco/config/
+
+my_run=/share/apps/busco-v2/BUSCO.py
+my_lineage=/home/eoziolor/phgenome/data/busco/actinopterygii_odb9
+my_genome=/home/eoziolor/phgenome/data/transcriptome/flnc_new.fasta
+my_out=arks_busco_flnc_actino
+
+
+python $my_run \
+-i $my_genome \
+-o $my_out \
+-l $my_lineage \
+-m transcriptome \
+-c 16 \
+-sp zebrafish
