a803954a92bd598f38ace44d2ca0823c038b9379
Elias Michel Oziolor <oziolor@gmail.com>
2018-09-06 15:15:40 -0700

    repeating repeatmodeler...get it?

--- scripts/repeat/modeler_phgenome.sh
+++ scripts/repeat/modeler_phgenome.sh
@@ -6,7 +6,7 @@
 #SBATCH -N 1
 #SBATCH -n 16
 #SBATCH --mem=60000
-#SBATCH --time=1-00:00
+#SBATCH --time=2-00:00
 #SBATCH --no-requeue
 #SBATCH -p high
 #SBATCH -D /home/eoziolor/phgenome/data/repeat/
@@ -15,8 +15,10 @@ source ~/.bashrc
 
 my_model=/home/eoziolor/program/RepeatModeler-open-1.0.11/RepeatModeler
 my_db=/home/eoziolor/phgenome/data/repeat/cpallasii
+my_olddir=/home/eoziolor/phgenome/data/repeat/RM_27835.WedSep51440122018/
 
 $my_model \
 -engine ncbi \
 -pa 15 \
+-recoverDir $my_olddir \
 -database $my_db
